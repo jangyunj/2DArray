6b12e50bfe4658ad1773fc04181aca2e793eea35
Yun Jang <jang.yunj7@gmail.com>
2023-05-30 23:35:14 -0400

    array

--- main.cpp
+++ main.cpp
@@ -20,17 +20,17 @@ int main()
     std::cout << cars[2][1] << " ";
     std::cout << cars[2][2] << '\n';
 
-    // Nested loops can also be used
+    // // Nested loops can also be used
 
-    int rows = sizeof(cars) / sizeof(cars[0]);
-    int columns = sizeof(cars[0]) / sizeof(cars[0][0]);
+    // int rows = sizeof(cars) / sizeof(cars[0]);
+    // int columns = sizeof(cars[0]) / sizeof(cars[0][0]);
 
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            std::cout << cars[i][j] << " ";
-        }
-        std::cout << '\n';
-    }
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     for (int j = 0; j < columns; j++)
+    //     {
+    //         std::cout << cars[i][j] << " ";
+    //     }
+    //     std::cout << '\n';
+    // }
 }
